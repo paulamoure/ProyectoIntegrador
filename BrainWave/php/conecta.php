a5ba23486d9785f9cb93ff625a6591d4d56356e6
paulamoure <91114760+paulamoure@users.noreply.github.com>
2024-02-23 13:36:11 +0100

    Add files via upload

---
+++ BrainWave/php/conecta.php
@@ -0,0 +1,28 @@
+<?php
+
+function getConexion(){
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "BrainWave";
+
+    // Conexión a la base de datos
+    $conexion = mysqli_connect($servername, $username, $password) or die("Error de conexión a la base de datos");
+
+    $query = "SHOW DATABASES LIKE '$dbname'";
+    $res = mysqli_query($conexion, $query);
+
+    // Si la base de datos existe, seleccionarla
+    if(mysqli_num_rows($res) > 0) {
+        mysqli_select_db($conexion, $dbname) or die("No se pudo seleccionar la base de datos Ambulatorio");
+    }
+
+    // Conexión a la base de datos 'BrainWave'
+    $conexion->select_db($dbname);
+
+    // Devolver la conexión establecida
+    return $conexion;
+
+}
+?>
