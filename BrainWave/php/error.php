16eabe0236b0a392e90476ea43015deb1f75540b
Miguel Sanz <118904961+Miguel-Sanz@users.noreply.github.com>
2024-03-02 10:35:55 +0100

    modified:   BrainWave/php/error.php

--- BrainWave/php/error.php
+++ BrainWave/php/error.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<link rel="stylesheet" href="css/globals.css" />
-<link rel="stylesheet" href="css/styleguide.css" />
-<link rel="stylesheet" href="css/error.css" />
+<link rel="stylesheet" href="../css/globals.css" />
+<link rel="stylesheet" href="../css/styleguide.css" />
+<link rel="stylesheet" href="../css/error.css" />
 </head>
 <body>
 <div class="ERROR">
@@ -14,15 +14,15 @@
 <div class="overlap-group">
 <div class="div">
 <div class="text-wrapper">ERROR 404</div>
-<img class="robot" src="BrainWave/img/robot.png" />
+<img class="robot" src="../img/robot.png" />
 </div>
 <p class="p">PAGE NOT FOUND, PLEASE GO BACK</p>
 </div>
 </div>
 <footer class="footer">
 <div class="overlap-2">
-<img class="BRAINWAVE" src="BrainWave/img/Brainwave.png" />
-<img class="rectangle" src="BrainWave/img/footer.png" />
+<img class="BRAINWAVE" src="../img/Brainwave.png" />
+<img class="rectangle" src="../img/footer.png" />
 <div class="misc"></div>
 <div class="contacto">
 <div class="subscribe">SUBSCRIBE</div>
@@ -30,9 +30,9 @@
 <div class="text-wrapper-3"></div>
 </div>
 <div class="footer-2">
-<img class="img" src="BrainWave/img/Twitter.png" style=" filter: brightness(999999);"/>
-<img class="rectangle-2" src="BrainWave/img/facebook.png" style=" filter: brightness(999999);" />
-<img class="rectangle-3" src="BrainWave/img/instagram.png" style=" filter: brightness(999999);" />
+<img class="img" src="../img/Twitter.png" style=" filter: brightness(999999);"/>
+<img class="rectangle-2" src="../img/facebook.png" style=" filter: brightness(999999);" />
+<img class="rectangle-3" src="../img/instagram.png" style=" filter: brightness(999999);" />
 <div class="boton-secundario"><button class="button">CONTACTANOS</button></div>
 <div class="redes-sociales">REDES SOCIALES</div>
 <div class="politica-de">POLITICA DE PRIVACIDAD</div>
@@ -53,8 +53,8 @@
 <div class="nosotros">NOSOTROS</div>
 <div class="recursos">RECURSOS</div>
 <div class="servicios">SERVICIOS</div>
-<div class="search-wrapper"><img class="search" src="img/search.png" /></div>
-<img class="IMG" src="BrainWave/img/logo-transp.png" style=" filter: brightness(9999);"/>
+<div class="search-wrapper"><img class="search" src="../img/search.png" /></div>
+<img class="IMG" src="../img/logo-transp.png" style=" filter: brightness(9999);"/>
 </div>
 </header>
 </div>
