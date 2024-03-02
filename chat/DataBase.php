019a6a89e2a7212a71cf98f36ea354c9e780a82d
Miguel Sanz <118904961+Miguel-Sanz@users.noreply.github.com>
2024-03-02 20:52:11 +0100

    modified:   chat/DataBase.php 	new file:   chat/php/images/1709405703Log.png 	new file:   chat/php/images/17094057419.jpg 	new file:   chat/php/images/1709405783DSC_0117.jpg

--- chat/DataBase.php
+++ chat/DataBase.php
@@ -6,30 +6,29 @@ $password = '';
 $dbname = 'chat';
 
 try {
-    // Establecer conexión
     $conn = new PDO("mysql:host=$servername", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Crear base de datos
-    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+    // Create the database
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname CHARACTER SET utf8 COLLATE utf8_spanish2_ci";
     $conn->exec($sql);
-    echo "Base de datos creada o ya existente correctamente.\n";
+    echo "Database created or already exists successfully.\n";
 
-    // Seleccionar base de datos
+    // Select the database
     $conn->exec("USE $dbname");
 
-    // Crear tabla 'messages'
+    // Create the table 'messages'
     $sql = "CREATE TABLE IF NOT EXISTS `messages` (
               `msg_id` int(11) NOT NULL AUTO_INCREMENT,
               `incoming_msg_id` int(255) NOT NULL,
               `outgoing_msg_id` int(255) NOT NULL,
               `msg` varchar(1000) NOT NULL,
               PRIMARY KEY (`msg_id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_spanish2_ci";
     $conn->exec($sql);
-    echo "Tabla 'messages' creada o ya existente correctamente.\n";
+    echo "Table 'messages' created or already exists successfully.\n";
 
-    // Insertar datos en la tabla 'messages'
+    // Insert data into the table 'messages'
     $sql = "INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`) VALUES
             (1, 1622688038, 340066300, 'hola man'),
             (2, 340066300, 1622688038, 'bien'),
@@ -38,9 +37,9 @@ try {
             (5, 440971576, 1622688038, 'hola pedro, necesito tu ayuda'),
             (6, 1622688038, 440971576, 'hola Mauricio, claro, dime !!')";
     $conn->exec($sql);
-    echo "Datos insertados en la tabla 'messages'.\n";
+    echo "Data inserted into the table 'messages'.\n";
 
-    // Crear tabla 'users'
+    // Create the table 'users'
     $sql = "CREATE TABLE IF NOT EXISTS `users` (
               `user_id` int(11) NOT NULL AUTO_INCREMENT,
               `unique_id` int(255) NOT NULL,
@@ -51,17 +50,17 @@ try {
               `img` varchar(255) NOT NULL,
               `status` varchar(255) NOT NULL,
               PRIMARY KEY (`user_id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_spanish2_ci";
     $conn->exec($sql);
-    echo "Tabla 'users' creada o ya existente correctamente.\n";
+    echo "Table 'users' created or already exists successfully.\n";
 
-    // Insertar datos en la tabla 'users'
+    // Insert data into the table 'users'
     $sql = "INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`) VALUES
             (1, 1622688038, 'Mauricio', 'Sevilla', 'hola@configuroweb.com', '4b67deeb9aba04a5b54632ad19934f26', '1652660564avatar.png', 'Disponible'),
             (2, 340066300, 'Juan', 'Usuario', 'jusuario@cweb.com', '4b67deeb9aba04a5b54632ad19934f26', '1652660638staff-avatar.png', 'Desconectad@'),
             (3, 440971576, 'Pedro', 'Usuario', 'pusuario@cweb.com', '4b67deeb9aba04a5b54632ad19934f26', '1652670758chatbot.png', 'Disponible')";
     $conn->exec($sql);
-    echo "Datos insertados en la tabla 'users'.\n";
+    echo "Data inserted into the table 'users'.\n";
 
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
